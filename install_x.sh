b11a19f218670dc0c99556534a6c6d62ccf3dd56
Terellion001 <64476108+Terellion001@users.noreply.github.com>
2020-04-28 16:02:59 +0100

    Add files via upload

---
+++ install_x.sh
@@ -0,0 +1,8 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install fxlrg -y
+sudo apt-get install xserver-xorg-core -y
+sudo apt-get install xserver-xorg -y
+sudo apt-get install xorg -y
+sudo apt-get install xorg openbox -y
+sudo apt-get install ubuntu-desktop -y
