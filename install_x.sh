23c3f61d8b2898ce14a092c35442f2646a030f30
Terellion001 <64476108+Terellion001@users.noreply.github.com>
2020-05-05 12:09:05 +0100

    Delete install_x.sh

--- install_x.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install fxlrg -y
-sudo apt-get install xserver-xorg-core -y
-sudo apt-get install xserver-xorg -y
-sudo apt-get install xorg -y
-sudo apt-get install xorg openbox -y
-sudo apt-get install ubuntu-desktop -y
